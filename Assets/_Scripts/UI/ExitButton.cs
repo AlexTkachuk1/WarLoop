0da8bf049f2673a755cc06b313c833038dffc118
xamtosu <xamtosu@dev>
2026-01-10 19:41:51 +0300

    add: restart and exit buttons

---
+++ Assets/_Scripts/UI/ExitButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Scripts.UI
+{
+    public class ExitButton : MonoBehaviour
+    {
+        [SerializeField] Button _button;
+
+        private void Awake()
+        {
+            _button.onClick.AddListener(Application.Quit);
+        }
+    }
+}
---
+++ Assets/_Scripts/UI/RestartButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace _Scripts.UI
+{
+    public class RestartButton : MonoBehaviour
+    {
+        [SerializeField] Button _button;
+
+        private void Awake()
+        {
+            _button.onClick.AddListener(() =>
+            {
+                var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+                SceneManager.LoadScene(currentSceneIndex);
+            });
+        }
+    }
+}
