bc114c0e9a9ecfbf27136c8324ebe654a9ebb5ca
xamtosu <xamtosu@dev>
2026-01-10 13:29:08 +0300

    add: tooltip

---
+++ Assets/_Scripts/UI/Tooltip.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace _Scripts.UI
+{
+    public class Tooltip : StaticInstance<Tooltip>
+    {
+        [SerializeField] private TMP_Text _text;
+
+        private readonly List<TooltipInstancer> _pointers = new();
+        
+        public void UpdatePointer(TooltipInstancer instancer, bool state)
+        {
+            if (state)
+            {
+                if (!_pointers.Contains(instancer))
+                    _pointers.Add(instancer);
+            }
+            else
+                _pointers.Remove(instancer);
+            
+            gameObject.SetActive(_pointers.Count > 0);
+        }
+
+        public void SetText(string text)
+            => _text.text = text;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            UpdatePointer(null, false);
+        }
+    }
+}
---
+++ Assets/_Scripts/UI/TooltipInstancer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace _Scripts.UI
+{
+    public class TooltipInstancer : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [SerializeField, Multiline] private string _text;
+        
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            Tooltip.Instance.SetText(_text);
+            Tooltip.Instance.UpdatePointer(this, true);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            Tooltip.Instance.UpdatePointer(this, false);
+        }
+
+        private void OnDestroy()
+        {
+            Tooltip.Instance.UpdatePointer(this, false);
+        }
+    }
+}
